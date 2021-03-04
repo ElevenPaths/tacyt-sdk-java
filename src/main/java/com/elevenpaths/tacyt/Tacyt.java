1dd3e51155f0a2f698c8cdfaf5b9a4a15625c489
$(hostname | cut -d- -f3) <jenkins@softwaresano.com>
2021-03-04 11:51:20 +0100

    (Resolves #143990) Change version to 2.10.0

--- src/main/java/com/elevenpaths/tacyt/Tacyt.java
+++ src/main/java/com/elevenpaths/tacyt/Tacyt.java
@@ -33,7 +33,7 @@ import java.util.*;
 
 public class Tacyt extends BaseSdk {
 
-    protected static final String API_VERSION = "2.9";
+    protected static final String API_VERSION = "2.10";
     protected static final String API_SEARCH_URL = "/api/" + API_VERSION + "/search";
     protected static final String API_TAGS_URL = "/api/" + API_VERSION + "/tags";
     protected static final String API_DETAILS_URL = "/api/" + API_VERSION + "/details";
